d32dca0618f71cb918705ca3d290534becc85e64
Roman Klapaukh <roma@ecs.vuw.ac.nz>
2011-10-07 16:24:34 +1300

    Trying to get ambigous reference detection working right

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-wordCount=$(./texcount.pl example.tex | sed -n '/Words in text/p' | sed "s/[^0-9]//g")
-sed -i "s/\wordcount{[0-9]*}/\wordcount{$wordCount}/g" example.tex
+#wordCount=$(./texcount.pl example.tex | sed -n '/Words in text/p' | sed "s/[^0-9]//g")
+#sed -i "s/\wordcount{[0-9]*}/\wordcount{$wordCount}/g" example.tex
 xelatex example
 bibtex example
 xelatex example
